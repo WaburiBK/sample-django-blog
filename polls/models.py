eed47d8f3afb3325a8a49a46c4f78e70cd7725e0
WaburiBK <bwaburi007@gmail.com>
2022-02-04 19:31:03 +0300

    ammended models

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# A model i the single, definitive source of information about my data
+# A model is the single, definitive source of information about my data
 # It contains the essential fields and behaviours of data being stored
 
 class Question(models.Model):
@@ -12,7 +12,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 #field instances tell Django what type of data each field holds
 # example: CharField holds character fields, DateTimeField for datetimes
