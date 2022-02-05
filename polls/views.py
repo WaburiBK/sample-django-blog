7f3f68d02b7904116c574d9acde75f7b3f1aa841
WaburiBK <bwaburi007@gmail.com>
2022-02-05 07:51:14 +0300

    spell check

--- polls/views.py
+++ polls/views.py
@@ -31,6 +31,7 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})"""
 
+# Amend views to incorporate less code using generic views
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
